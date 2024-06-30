9c4ce616259f2ff1c185c35bca10cf4ce1e6dcfb
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-06-30 05:58:46 -0700

    Review and Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,10 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+# Install werkzeug requirement
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
+}
