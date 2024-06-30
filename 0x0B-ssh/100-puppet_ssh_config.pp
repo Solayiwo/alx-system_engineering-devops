2fd34f0e7ce5e4af3f5367c3b0eb356a42aa9d34
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-06-30 06:53:02 -0700

    Puppet SSH client configuration

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Client configuration file using Puppet
+file {'/etc/ssh/shh_config':
+	ensure  => 'present',
+}
+
+file_line {'Turn off passwd auth':
+	path    => '/etc/ssh/ssh_config',
+	line    => 'PasswordAuthentication no',
+	match   => 'PasswordAuthentication yes',
+	replace => 'true',
+	
+}
+file_line {'Declare identity file':
+	path    => '/etc/ssh/ssh_config',
+	line    => 'IdentityFile ~/.ssh/school',
+	match   => '^IdentityFile',
+	ensure  => 'present',
+}
