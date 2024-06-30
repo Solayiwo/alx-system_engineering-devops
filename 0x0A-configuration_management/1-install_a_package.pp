8fd82aa789f4e4c5f988f27a0ef9872c756c83bb
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-06-30 06:05:01 -0700

    Review and Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 # Install a specific version of flask (2.1.0)
-package { 'flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 
 # Install werkzeug requirement
