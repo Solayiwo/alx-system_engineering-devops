b9e796337a6517cb1388f09f3b60c5d60a4185a5
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-08-20 16:56:57 -0700

    Increase Ulimit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Increasing ULIMIT default file for the number of traffic an Nginx server can handle
+
+exec { 'fix--for-nginx':
+  command  => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path     => '/usr/local/bin/:/bin/',
+}
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command  => '/etc/init.d/nginx restart',
+  path     => '/etc/init.d/',
+}
